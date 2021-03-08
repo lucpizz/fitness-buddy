e5b0dc4d3781f9639198b6c8f206b3fdd3c92f09
lucpizz <lucpizz@gmail.com>
2021-03-07 23:53:55 -0500

    update

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -33,7 +33,7 @@ router.get("/api/workouts", (req, res) => {
 
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({})
-    //.sort({ date: -1 })
+    .sort({ date: -1 })
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
